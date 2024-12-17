2e7ef3f0baafa5144f3751b21f37b6de2e9767ba
cassie <cassandrafeinberg@gmail.com>
2024-12-17 10:57:30 -0500

    v2

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 async function getData() {
-    const response = await           
-    fetch("ZonAnn.Ts+dSST.csv");
+  try {
+    const response = await fetch("ZonAnn.Ts+dSST.csv");
     const data = await response.text();
     const rows = data.split("\n").slice(1);
+
+    const years = [];
+    const temps = [];
+
     rows.forEach((elem) => {
-              const row = elem.split(",");
-              const year = row[0];
-              const temp = row[1];
-              console.log(year, temp);
-            });
-          }
-    //end of functions
-    //call the function to test if you see the image on the left in the console
-    
+      const row = elem.split(",");
+      const year = row[0];
+      let temp = parseFloat(row[1]);
+      temp += 14;
+      years.push(year);
+      temps.push(temp);
+    });
+
+    createLineChart(years, temps);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+function createLineChart(years, temps) {
+  const ctx = document.getElementById('lineChart');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: years,
+      datasets: [{
+        label: 'Global Temperatures',
+        backgroundColor: 'red',
+        data: temps,
+        borderWidth: 1,
+      }]
+    },
+    options: {
+
+    }
+  });
+}
